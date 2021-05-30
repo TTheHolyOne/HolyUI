2322d1b7e18aace48b262ee04a74c45fe0e9c5c7
TTheHolyOne <69742571+TTheHolyOne@users.noreply.github.com>
2021-05-30 02:31:27 -0500

    Added UI

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolyUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form2();
+            Hide();
+            form2.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+            btn_install.Text = "Installing..";
+            Program.startprogram();
+            btn_install.Text = "Done Installing!";
+        }
+
+        private void btn_quit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+/*
+ * Created by TTheHolyOne#1642
+ * Holy Tools - UI Template
+ * Please enjoy
+ * The "startprogram" method is the code that runs when you click Install HolyTools
+ * Please feel free to change that to anything you would like
+ * Enjoy <3
+ * I might post the code on github havent decided yet
+ * My github is github.com/ttheholyone
+ * My youtube is youtube.com/ttheholyone
+ * my website is skids.host
+ */
+
+namespace HolyUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+
+
+        public static void startprogram()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile("https://cdn.discordapp.com/attachments/810071107990061060/848455700345782282/Read_Me.txt", "Read_Me.txt");
+                webClient.DownloadFile("https://cdn.discordapp.com/attachments/810071107990061060/848454575723642920/HolyTools.exe", "HolyTools.exe");
+            }
+            Process.Start("HolyTools.exe");
+            Console.WriteLine("Thank you..");
+            System.Diagnostics.Process.Start("https://skids.host/install");
+        }
+    }
+}
